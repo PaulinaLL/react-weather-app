9ea279fd25f572f13d8ba506b88ebf9bf2f2c4ab
PaulinaLL <paulinalll90@wp.pl>
2020-05-12 22:27:25 +0200

    Add func getWeatherData to fetch data

--- src/App.js
+++ src/App.js
@@ -1,12 +1,81 @@
 import React from "react";
 import "./App.scss";
 
-function App() {
-  return (
-    <div className="App">
-      <main className="App-main"></main>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      status: "init",
+      isLoaded: false,
+      weatherData: null,
+    };
+  }
+
+  abortController = new AbortController();
+  controllerSignal = this.abortController.signal;
+
+  getWeatherData = () => {
+    const weatherApi = `http://api.openweathermap.org/data/2.5/weather?q=London&units=metric&appid=${process.env.REACT_APP_WEATHER_KEY}`;
+
+    fetch(weatherApi, { signal: this.controllerSignal })
+      .then((response) => response.json())
+      .then(
+        (result) => {
+          console.log(result);
+
+          const { name } = result;
+          const { country } = result.sys;
+          const {
+            temp,
+            temp_min,
+            temp_max,
+            feels_like,
+            humidity,
+          } = result.main;
+          const { description, icon } = result.weather[0];
+          const { speed, deg } = result.wind;
+
+          this.setState({
+            isLoaded: true,
+            weatherData: {
+              name,
+              country,
+              description,
+              icon,
+              temp: temp.toFixed(1),
+              feels_like: feels_like.toFixed(1),
+              temp_min: temp_min.toFixed(1),
+              temp_max: temp_max.toFixed(1),
+              speed,
+              deg,
+              humidity,
+            },
+          });
+        },
+        (error) => {
+          this.setState({
+            isLoaded: true,
+            error,
+          });
+        }
+      );
+  };
+
+  componentDidMount() {
+    this.getWeatherData();
+  }
+
+  componentWillUnmount() {
+    this.abortController.abort();
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <main></main>
+      </div>
+    );
+  }
 }
 
 export default App;

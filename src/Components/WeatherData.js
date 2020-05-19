5f30db61d85165eef4d0505220978a7268a96362
PaulinaLL <paulinalll90@wp.pl>
2020-05-19 23:35:51 +0200

    Replace dummy data with fetched data, import icons

--- src/Components/WeatherData.js
+++ src/Components/WeatherData.js
@@ -1,45 +1,69 @@
 import React from "react";
+import { DegreesToDirection, Month, Weekday, Day } from "../helpers/utils";
 import { Clock } from "../Components/Clock";
+import clockIcon from "../images/clock.png";
+import dropIcon from "../images/drop.png";
+import windIcon from "../images/wind.png";
 
-export const WeatherData = () => {
+export const WeatherData = ({ data }) => {
+  const {
+    name,
+    country,
+    temp,
+    description,
+    temp_min,
+    temp_max,
+    icon,
+    feels_like,
+    speed,
+    deg,
+    humidity,
+  } = data;
   return (
     <>
       <header>
         <div>
-          <img src={require(`../images/clock.png`)} alt="time icon" />
+          <img src={clockIcon} alt="time icon" />
           <Clock />
         </div>
-        <h5>Tuesday, January 13</h5>
+        <h5>
+          {Weekday}, {Month} {Day}
+        </h5>
       </header>
       <main>
         <div className="weather-main">
           <img
-            src={`http://openweathermap.org/img/wn/10d@2x.png`}
+            src={`http://openweathermap.org/img/wn/${icon}@2x.png`}
+            alt="weather icon"
             alt="weather icon"
             className="weather-icon"
           />
           <div>
-            <h2>Athens, GR</h2>
-            <h3 className="description">Light Rain</h3>
+            <h2>
+              {name}, {country}
+            </h2>
+            <h3 className="description">{description}</h3>
           </div>
         </div>
         <div className="temp-main">
-          <h5>Feels like 9°</h5>
-          <h1 className="temperature">12°</h1>
+          <h5>Feels like {feels_like} °</h5>
+          <h1 className="temperature">{temp}°</h1>
           <div className="hi-lo">
-            <h5>H 16°</h5>
-            <h5>L 7°</h5>
+            <h5>H {temp_max}°</h5>
+            <h5>L {temp_min}°</h5>
           </div>
         </div>
       </main>
       <footer>
         <div className="weather-prop">
-          <img src={require("../images/wind.png")} alt="" />
-          <h4>SE 2.3 KPH</h4>
+          <img src={windIcon} alt="wind-icon" />
+          <h4>
+            {DegreesToDirection(deg)} {speed} KPH
+          </h4>
         </div>
         <div className="weather-prop">
-          <img src={require("../images/drop.png")} alt="" />
-          <h4>72 %</h4>
+          <img src={dropIcon} alt="drop-icon" />
+          <h4>{humidity} %</h4>
         </div>
       </footer>
     </>
